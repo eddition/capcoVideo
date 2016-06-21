4b3a49dfae918137b50bc31ffaebb9dc091c4617
Eddie Shin <eshin0910@gmail.com>
2016-06-21 16:44:20 -0400

    added more video thumbs and adjusted stylying

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -40,6 +40,54 @@ angular.module('capcoVideoApp')
 	        "code":'<iframe width="960" height="540" src="http://www.youtube.com/embed/gimGOlbnNjk" frameborder="0" allowfullscreen></iframe>',
 	        "tags":["funny", "client"],
 	        "thumbnail": "/images/thumbnails/thumb5.jpg"
+	    },
+	    {
+	        "title":"The Dark Knight Rises - Official Trailer 3 [HD]",
+	        "code":"<iframe width='960' height='540' src='http://www.youtube.com/embed/9l3DDSXkEQ0' frameborder='0' allowfullscreen></iframe>",
+	        "tags":["funny", "internal"],
+	        "thumbnail": "/images/thumbnails/thumb6.jpg"
+	    },
+	    {
+	        "title":"The Dark Knight Rises - Official Trailer 2 [HD]",
+	        "code":"<iframe width='960' height='540' src='http://www.youtube.com/embed/1KgCP0QyfdY' frameborder='0' allowfullscreen></iframe>",
+			"tags":["sales", "internal"],
+			"thumbnail": "/images/thumbnails/thumb7.jpg"
+	    },
+	    {
+	        "title":"The Dark Knight Rises - Official Teaser Trailer [HD]",
+	        "code":"<iframe width='960' height='540' src='http://www.youtube.com/embed/kqF8lcKTLw0' frameborder='0' allowfullscreen></iframe>",
+			"tags":["client", "internal"],
+			"thumbnail": "/images/thumbnails/thumb8.jpg"
+	    },
+	    {
+	        "title":"The Dark Knight Rises - Official Rise TV Spot [HD]",
+	        "code":"<iframe width='960' height='540' src='http://www.youtube.com/embed/fNGAYD31fMo' frameborder='0' allowfullscreen></iframe>",
+			"tags":["funny", "internal"],
+			"thumbnail": "/images/thumbnails/thumb9.jpg"
+	    },
+	    {
+	        "title":"The Dark Knight Rises - Official Back TV Spot [HD]",
+	        "code":'<iframe width="960" height="540" src="http://www.youtube.com/embed/gimGOlbnNjk" frameborder="0" allowfullscreen></iframe>',
+	        "tags":["funny", "client"],
+	        "thumbnail": "/images/thumbnails/thumb10.jpg"
+	    },
+	    	    {
+	        "title":"The Dark Knight Rises - Official Trailer 3 [HD]",
+	        "code":"<iframe width='960' height='540' src='http://www.youtube.com/embed/9l3DDSXkEQ0' frameborder='0' allowfullscreen></iframe>",
+	        "tags":["funny", "internal"],
+	        "thumbnail": "/images/thumbnails/thumb11.jpg"
+	    },
+	    {
+	        "title":"The Dark Knight Rises - Official Trailer 2 [HD]",
+	        "code":"<iframe width='960' height='540' src='http://www.youtube.com/embed/1KgCP0QyfdY' frameborder='0' allowfullscreen></iframe>",
+			"tags":["sales", "internal"],
+			"thumbnail": "/images/thumbnails/thumb12.jpg"
+	    },
+	    {
+	        "title":"The Dark Knight Rises - Official Teaser Trailer [HD]",
+	        "code":"<iframe width='960' height='540' src='http://www.youtube.com/embed/kqF8lcKTLw0' frameborder='0' allowfullscreen></iframe>",
+			"tags":["client", "internal"],
+			"thumbnail": "/images/thumbnails/thumb13.jpg"
 	    }
 	]
 
